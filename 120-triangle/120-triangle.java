8df0146ef352c4bac9ba1688b3e53de44b1fcfca
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-07 15:33:44 +0530

    Time: 6 ms (42.87%), Space: 44 MB (37.45%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int ans = Integer.MAX_VALUE;
+        int cols;
+        List<Integer> row;
+        for(int i=1;i<triangle.size();i++){
+            cols = triangle.get(i).size();
+            row = triangle.get(i);
+            for(int j=0;j<cols;j++){
+                //System.out.println(row.get(j));
+                if(j==0){
+                    row.set(j,row.get(j) + triangle.get(i-1).get(j));
+                }else if(j==cols-1){
+                    row.set(j,row.get(j) + triangle.get(i-1).get(j-1));
+                }else{
+                    row.set(j,row.get(j) + Math.min(triangle.get(i-1).get(j-1),triangle.get(i-1).get(j)));
+                }
+            }
+        }
+        for(int j = 0;j<triangle.size();j++){
+            if(triangle.get(triangle.size()-1).get(j)<ans){
+                ans = triangle.get(triangle.size()-1).get(j);
+            }
+        }
+        return ans;
+    }
+}
