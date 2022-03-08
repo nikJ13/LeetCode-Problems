f486e9fb6ac0bdb2a36457464b2e33f316b9c717
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-08 17:34:36 +0530

    Time: 8 ms (91.01%), Space: 69.3 MB (72.55%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -11,6 +11,7 @@ class Solution {
                 ans[i][j] = Integer.MAX_VALUE-100000;
             }
         }
+        // First iterating from top-left to bottom-right and for each cell, we compare the distances of zeroes of the cells from top and left of that cell
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
@@ -25,7 +26,7 @@ class Solution {
                 }
             }
         }
-        //System.out.println(mat[2][0]);
+        //Iterating from bottom-right to top-left; comparing each cell with the min distances from zeroes from bottom and right of that particular cell
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                     if(i<n-1){
