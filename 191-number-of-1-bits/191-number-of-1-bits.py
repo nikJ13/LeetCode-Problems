4722d2f263e3c1afd17aabee7e820884687e7092
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-08 13:24:16 +0530

    Time: 44 ms (54.49%), Space: 13.8 MB (68.55%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -2,8 +2,6 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count,right = 0,0
         while n:
-            right = n & 1
-            if right==1:
-                count += 1
-            n = n >> 1
+            count += (n & 1)   # taking the right most bit of the binary number
+            n = n >> 1  # right shifting by one
         return count
