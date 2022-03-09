55e0533afe35b7c4fe7804db83f531af90318a2e
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-09 22:55:30 +0530

    Time: 4 ms (24.68%), Space: 58.4 MB (9.51%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -3,14 +3,14 @@ class Solution {
         int[] ans = new int[nums.length];
         ans[0] = 1;
         int temp = 0;
-        for(int i=1;i<nums.length;i++){
+        for(int i=1;i<nums.length;i++){   //iterate from the front side, calculating the product of numbers before the current number 
             if(i==1){
                 ans[i] = nums[i-1];
             }else{
                 ans[i] = ans[i-1]*nums[i-1];
             }
         }
-        for(int i=nums.length-1;i>=0;i--){
+        for(int i=nums.length-1;i>=0;i--){ //iterate from the backside, calculating the product of numbers after the current number
             if(i==nums.length-1){
                 temp = 1;
             }else{
