3da98afe20a399a74561e96fa1acfe2726196ae5
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-17 13:03:18 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (26.16%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -8,8 +8,8 @@ class Solution {
         if(dp[index]!=-1){
             return dp[index];
         }
-        for(int i=index+2;i<nums.length;i++){
-            dp[index] = Math.max(nums[index]+helper(nums,i),dp[index]);
+        for(int i=index+2;i<nums.length;i++){       
+            dp[index] = Math.max(nums[index]+helper(nums,i),dp[index]); //check if the current steal+steal from other houses is greater than the steal specified in the dp array
         }
         return dp[index];
     }
@@ -19,10 +19,10 @@ class Solution {
         }
         dp = new int[nums.length];
         int ans = 0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){ //initialize the dp array to '-1' each
             dp[i] = -1;
         }
-        for(int j=0;j<nums.length;j++){
+        for(int j=0;j<nums.length;j++){ //with different starting points
             if(dp[j]==-1){
             ans = Math.max(helper(nums,j),ans);
             }
