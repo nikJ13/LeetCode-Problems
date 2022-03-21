552c7731d4008c75db21536229405c4f6deec9b4
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-21 12:17:29 +0530

    Time: 5 ms (34.67%), Space: 47.2 MB (49.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,25 +1,19 @@
 class Solution {
-    public static int dfs(int[][] visited,int[][] grid,int x,int y,int n,int m){
-        if(x<0||x>=n||y<0||y>=m||visited[x][y]==1||grid[x][y]==0){
+    public int helper(int[][] grid, int x, int y, int n, int m){
+        if(x>=n||y>=m||x<0||y<0||grid[x][y]==-1||grid[x][y]==0){
             return 0;
         }
-        visited[x][y] = 1;
-        return 1+dfs(visited,grid,x-1,y,n,m)+dfs(visited,grid,x,y-1,n,m)+dfs(visited,grid,x+1,y,n,m)+dfs(visited,grid,x,y+1,n,m);
+        grid[x][y] = -1;
+        return 1 + helper(grid,x+1,y,n,m) + helper(grid,x,y+1,n,m) + helper(grid,x-1,y,n,m) + helper(grid,x,y-1,n,m);
     }
     public int maxAreaOfIsland(int[][] grid) {
+        int ans = 0;
         int n = grid.length;
         int m = grid[0].length;
-        int[][] visited = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                visited[i][j] = 0;
-            }
-        }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(visited[i][j]==0 && grid[i][j]==1){
-                    ans = Math.max(dfs(visited,grid,i,j,n,m),ans);
+                if(grid[i][j]==1){
+                    ans = Math.max(ans,helper(grid,i,j,n,m));
                 }
             }
         }
