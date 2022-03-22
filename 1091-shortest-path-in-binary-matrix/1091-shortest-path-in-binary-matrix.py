9a68016974e8cab2e7fd656efaab101d747bcae8
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-22 12:44:34 +0530

    Time: 1884 ms (5.01%), Space: 14.3 MB (70.71%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0]==1 or grid[-1][-1]==1:
+            return -1
+        moves = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1)]
+        n = len(grid)
+        visited = [[-1 for _ in range(n)] for i in range(n)]
+        visited[0][0],que = 1,[(0,0)]
+        while que:
+            node = que.pop(0)
+            for i in range(8):
+                #print(node[0]+moves[i][0],node[1]+moves[i][1])
+                if node[0]+moves[i][0]<0 or node[0]+moves[i][0]>=n or node[1]+moves[i][1]<0 or node[1]+moves[i][1]>=n or grid[node[0]+moves[i][0]][node[1]+moves[i][1]]==1:
+                    continue
+                # elif grid[node[0]+moves[i][0]][node[1]+moves[i][1]]==1:
+                #     continue
+                if visited[node[0]+moves[i][0]][node[1]+moves[i][1]]==-1:
+                    visited[node[0]+moves[i][0]][node[1]+moves[i][1]] = visited[node[0]][node[1]] + 1
+                    que.append((node[0]+moves[i][0],node[1]+moves[i][1]))
+        return visited[n-1][n-1]
