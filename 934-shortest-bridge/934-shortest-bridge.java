9afd38341e1346e83c4913aa317a7cdb5dca93b3
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-22 14:48:58 +0530

    Time: 11 ms (66.86%), Space: 53.4 MB (71.98%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -12,7 +12,7 @@ class Solution {
     public int shortestBridge(int[][] grid) {
         int n = grid.length;
         int count = 2;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){   //marking both the islands with different colours (numbers i.e. '2' for the first island and '3' for the second one)
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
                     helper(grid,i,j,n,count);
@@ -23,7 +23,7 @@ class Solution {
                 break;
             }
         }
-        //System.out.println(grid[0][1]+" "+grid[2][2]);
+        // making a queue and initiliasing it with one of the island's cells; because we need to find the min distance between the first and the second island
         Queue<int[]> queue=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -40,13 +40,13 @@ class Solution {
                 for(int j=0;j<4;j++){
                     int newX = node[0] + directions[j][0];
                     int newY = node[1] + directions[j][1];
-                    if(newX<0||newX>=n||newY<0||newY>=n){
+                    if(newX<0||newX>=n||newY<0||newY>=n){ // checking the boundary conditions
                         continue;
                     }
-                    if(grid[newX][newY]==0){
+                    if(grid[newX][newY]==0){   // we have to measure the distance between islands by traversing only the cells with '0'
                         queue.add(new int[]{newX,newY,node[2]+1});
-                        grid[newX][newY] = 1;
-                    }else if(grid[newX][newY]==3){
+                        grid[newX][newY] = 1; // marking this cell as visited
+                    }else if(grid[newX][newY]==3){  // if the cell is equal to the other island, that means the other island has been found; return the node's distance
                         return node[2];
                     }
                 }
