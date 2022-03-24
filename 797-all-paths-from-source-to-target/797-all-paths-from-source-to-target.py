852e0a57e5d4aa5f5d06eb6915f6c54e51d70d79
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-24 13:38:48 +0530

    Time: 154 ms (36.40%), Space: 15.7 MB (78.00%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,17 +1,12 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         ans = []
-        vis = [0]*len(graph)
         def recurse(index,temp):
             if index==len(graph)-1:
-                #temp.append(index)
                 ans.append(temp[:])
                 return
             if len(graph[index])==0:
-                #print("wow")
                 return
-            #vis[index] = 1
-            #print("here",temp)
             for i in range(len(graph[index])):
                 temp.append(graph[index][i])
                 recurse(graph[index][i],temp)
