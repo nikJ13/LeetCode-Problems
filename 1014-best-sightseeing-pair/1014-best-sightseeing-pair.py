5fd2827be7df99bf3e26fa8f343a4a61847bf7b3
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-03-31 22:10:17 +0530

    Time: 500 ms (88.19%), Space: 20.6 MB (29.48%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        left_best = values[0] + 0
-        ans = left_best + values[1] - 1
+        #left_best element is the element that is the most max element (which comprises of value at a particular index + that particular index) to the left of the current index 
+        left_best = values[0] + 0 # calculating the elft_best element for index 1
+        ans = left_best + values[1] - 1 # initialising the ans variable according to the given equation in the question, which is greatest element to the left of the current index + current index value - current index
         for curr in range(2,len(values)):
-            left_best = max(left_best,values[curr-1]+curr-1)
-            ans = max(ans,left_best+values[curr]-curr)
+            left_best = max(left_best,values[curr-1]+curr-1) # the left_best element can either be the existing left_best element or the value at the index before the current index + previous index
+            ans = max(ans,left_best+values[curr]-curr) # ans can be either the existing ans or the current left_best element + value at current index - current index
         return ans
