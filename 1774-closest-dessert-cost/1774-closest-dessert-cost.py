e4adfae78ce6fab3b04ae4f996e0a41994ffa8d3
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-06 15:32:35 +0530

    Time: 1707 ms (29.42%), Space: 38.8 MB (6.44%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,5 +1,6 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
+        #when the question says closest to the target value, then the sum of the base + toppings can also be greater than the target value and be closer to it.
         costs = set()
         diff = 100000
         ans = 100000
@@ -9,12 +10,14 @@ class Solution:
                 costs.add(number)
                 diff = min(diff,abs(number-target))
                 return
+            # there are three options for each step, either don't add the particular topping, add one quantity of that topping or add two quantities of that topping; in all the three cases the index should be incremented to the next topping
             helper(number,index+1)
             helper(number+toppingCosts[index],index+1)
             helper(number+(toppingCosts[index]*2),index+1)
             
         for i in range(len(baseCosts)):
             helper(baseCosts[i],0)
+        # to find the lower number that has the same difference from the target than another higher number
         for j in costs:
             if abs(target-j)==diff:
                 ans = min(ans,j)
