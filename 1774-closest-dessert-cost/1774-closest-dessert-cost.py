547731feed812c298c8a0254083d3269a628e6de
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-06 15:24:46 +0530

    Time: 1460 ms (31.62%), Space: 38.7 MB (6.44%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,12 +1,10 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
         costs = set()
-        #print(dict1)
         diff = 100000
         ans = 100000
         def helper(number,index):
             nonlocal diff
-            #print(number,dict1)
             if index>=len(toppingCosts):
                 costs.add(number)
                 diff = min(diff,abs(number-target))
@@ -17,9 +15,7 @@ class Solution:
             
         for i in range(len(baseCosts)):
             helper(baseCosts[i],0)
-        #print(diff)
         for j in costs:
             if abs(target-j)==diff:
-                #print(ans)
                 ans = min(ans,j)
         return ans
