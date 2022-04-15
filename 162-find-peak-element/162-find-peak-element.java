81ae4427045c1635fef6c179109c89b9143b6d60
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-16 00:36:54 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (83.35%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,11 +1,11 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length-1;
-        while(left<right){
+        while(left<right){ //using binary search; check if the current mid is greater than the element to the right of it or not
             int mid = left + (right-left)/2;
-            if(nums[mid]<nums[mid+1]){
+            if(nums[mid]<nums[mid+1]){ //if lesser, that means the peak has to be somewhere on the right of the mid element
                 left = mid+1;
-            }else{
+            }else{ //if greater than or equal to, then the peak has to be somewhere on the left (and including itself) of the mid element
                 right = mid;
             }
         }
