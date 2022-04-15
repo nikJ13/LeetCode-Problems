4257b8b4a6d3c3cfe61a8bfa1fe2763f8fbc3015
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-15 21:26:53 +0530

    Time: 4 ms (100.00%), Space: 48.5 MB (83.18%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int x = 0, y = matrix[0].length-1;
         while(y>=0 && x<matrix.length){
-            if(matrix[x][y]<target){
+            if(matrix[x][y]<target){ //first check row wise if the target element falls lesser than the greatest element in the current row
                 x++;
-            }else if(matrix[x][y]>target){
+            }else if(matrix[x][y]>target){ //if the target element is lesser than the greatest element in that particular row, then that means the ans must be in that row itself; thus, decrease the col index
                 y--;
             }else{
                 return true;
