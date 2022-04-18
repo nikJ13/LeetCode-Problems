51dfda678928b07c3bfac11264373328089450d0
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-18 13:44:41 +0530

    Time: 35 ms (47.40%), Space: 46.6 MB (84.23%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -8,17 +8,20 @@ class Solution {
                 int end = nums.length-1;
                 int new_sum = 0-nums[i];
                 while(start<end){
-                    //System.out.println(nums[start]);
-                    //System.out.println(nums[end]);
+                    // System.out.println("start");
+                    // System.out.println(nums[i]);
+                    // System.out.println(nums[start]);
+                    // System.out.println(nums[end]);
+                    // System.out.println("end");
                     if(nums[start]+nums[end]==new_sum){
                         List<Integer> temp_arr = new ArrayList<>();
                         temp_arr.add(nums[i]);
                         temp_arr.add(nums[start]);
                         temp_arr.add(nums[end]);
                         ans.add(temp_arr);
+                        int temp = nums[end];
                         start++;
-                        end--;
-                        while(start<end && nums[end]==nums[end+1]){
+                        while(start<end && nums[end]==temp){
                             end--;
                         }
                     }else if(nums[start]+nums[end]<new_sum){
