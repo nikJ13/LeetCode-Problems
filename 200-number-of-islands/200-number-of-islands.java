af7d6041fa33686802ed91f83fa5c18aed3e8a8a
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-20 22:30:12 +0530

    Time: 6 ms (35.44%), Space: 57.5 MB (40.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,23 +1,25 @@
 class Solution {
-    public void helper(char[][] grid,int x,int y,int n,int m){
-        if(x<0 || x>=n || y<0 || y>=m || grid[x][y]=='2' || grid[x][y]=='0'){
+    
+    private void helper(char[][] grid, int x, int y, int m, int n){
+        if(x<0||x>=m||y<0||y>=n||grid[x][y]=='0'||grid[x][y]=='2'){
             return;
         }
         grid[x][y] = '2';
-        helper(grid,x+1,y,n,m);
-        helper(grid,x,y+1,n,m);
-        helper(grid,x-1,y,n,m);
-        helper(grid,x,y-1,n,m);
+        helper(grid,x+1,y,m,n);
+        helper(grid,x,y+1,m,n);
+        helper(grid,x-1,y,m,n);
+        helper(grid,x,y-1,m,n);
     }
     public int numIslands(char[][] grid) {
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                //System.out.println(grid[i][j]);
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    //System.out.println("yes");
-                    helper(grid,i,j,grid.length,grid[0].length);
-                    count ++;
+                    //System.out.println("hehe");
+                    helper(grid,i,j,m,n);
+                    count++;
                 }
             }
         }
