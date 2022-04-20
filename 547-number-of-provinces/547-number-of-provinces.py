b23be4d5077ef5e15e0835e9e61ce6332196c0b6
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-20 22:50:46 +0530

    Time: 369 ms (18.15%), Space: 14.5 MB (33.34%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        dict1 = {i:[] for i in range(n)}
+        matrix = isConnected
+        
+        def helper(dict1,visited,vertex):
+            if visited[vertex]==1 or len(dict1[vertex])==0:
+                return
+            visited[vertex] = 1
+            for neighbor in dict1[vertex]:
+                helper(dict1,visited,neighbor)
+        for i in range(n):
+            for j in range(n):
+                if i!=j and matrix[i][j]==1:
+                    dict1[i].append(j)
+                    dict1[j].append(i)
+        visited = [0]*n
+        ans = 0
+        for vertex in dict1:
+            if visited[vertex]==0:
+                helper(dict1,visited,vertex)
+                ans += 1
+        return ans
+                
