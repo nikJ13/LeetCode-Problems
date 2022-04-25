d7f9acf50af9ecaadf09eb980c7feb6a06ef917f
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-25 19:04:09 +0530

    Time: 76 ms (5.42%), Space: 14.3 MB (35.58%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        
+        def helper(nums,index,temp):
+            ans.append(temp[:])
+            for i in range(index,len(nums)):
+                temp.append(nums[i])
+                helper(nums,i+1,temp)
+                temp.pop()
+        helper(nums,0,[])
+        return ans
