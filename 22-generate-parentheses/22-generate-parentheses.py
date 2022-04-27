d875e061a355e55fdea8b2d8eaa25cfd20babf3f
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-04-28 00:21:10 +0530

    Time: 45 ms (61.08%), Space: 14.3 MB (42.04%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,8 +1,9 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
+        
+        # Here, nOpen and nClose are the number of open and closed brackets respectively. So at each position in the answer string, there are two options, either insert opening or closing brackets, and this is possible only if the number of those brackets left are not zero. Also, in order to make valid combinations, the stack counter should be there, so whenever there is a push, then increment its value, and if there is a pop, then check if the stack is zero (empty of not), if not so, then there is still scope to form valid string, otherwise it reverses back to the previous recursion
         def helper(nOpen,nClose,temp,stack):
-            #print(stack)
             if nOpen==0 and nClose==0:
                 ans.append(temp)
                 return
