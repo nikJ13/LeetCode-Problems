2a2d40e9b35f5a46a5844ab75d6674bcc211de06
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-05-01 20:41:33 +0530

    Time: 675 ms (45.31%), Space: 15.2 MB (52.93%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,7 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         start,destination = len(nums)-2,len(nums)-1 # this is greedy solution; here we take two pointers, and iterate from the back of the array
-        # at each start pointer, we check if that can be reached to the destination pointer or not; if so, then this start pointer becomes the new destination and the element before it becomes the new start element
+        # at each start pointer, we check if that can be reached to the destination pointer or not; if so, then this start pointer becomes the new destination and the element before it becomes the new start element; check the neetcode solution for it
         while start>=0:
             if start+nums[start]>=destination:
                 destination = start
