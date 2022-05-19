7c69239a69892eee2c5b95f8eb45ca0d6a18e9d5
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-05-19 22:34:20 +0530

    Time: 13 ms (81.30%), Space: 45.9 MB (84.62%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,15 +3,11 @@ class Solution {
         int[][] dp = new int[text1.length()+1][text2.length()+1];
         for(int i=1;i<text1.length()+1;i++){
             for(int j=1;j<text2.length()+1;j++){
-                //System.out.println(text1.charAt(i-1));
-                //System.out.println(text1.charAt(j-1));
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    //System.out.println(dp[i-1][j-1]++);
                     dp[i][j] = ++dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
-                //System.out.println(dp[i][j]);
             }
         }
         return dp[text1.length()][text2.length()];
