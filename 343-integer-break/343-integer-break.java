4b4aff647a0443b8c7f61a640c4df51f23719eb6
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-05-23 23:24:08 +0530

    Time: 2 ms (21.27%), Space: 40.9 MB (41.17%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -4,8 +4,8 @@ class Solution {
         dp[1] = 1;
         for(int i=2;i<n+1;i++){
             int max = Integer.MIN_VALUE;
-            for(int j=1;j<=i/2;j++){
-                int product = Math.max(dp[j],j)*Math.max(dp[i-j],i-j);
+            for(int j=1;j<=i/2;j++){ // we iterate till i by two because the numbers beyond that half will give the same product value
+                int product = Math.max(dp[j],j)*Math.max(dp[i-j],i-j); // here, for each number, we check if we can get either the max product by considering then current number itself or the max product for the current number
                 max = Math.max(max,product);
             }
             dp[i] = max;
