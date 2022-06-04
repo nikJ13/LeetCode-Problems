9527f6cab7d8c38621ca17c5b7bb3d3a6b659edf
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-06-04 13:51:58 +0530

    Time: 66 ms (65.90%), Space: 13.9 MB (39.39%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -4,7 +4,7 @@ class Solution:
         ans = 0
         posDiag = set()
         negDiag = set()
-        
+        # in this problem, every row has one queen and every column also has one queen, but none are placed on eachother's diagonals, otherwise, they would cancel out, thus, we recurse through every row, checking if the column of that particular row already has a queen or not, by maintaining a set; simultaneously, we check for the positive (Slope) diagonals and negative (slope) diagnonals if they are present in the reapective set already [for a point (r,c), (r+c) is the same for a particular positive slope and (r-c) is the same for a particular negative slope]
         def recurse(r):
             nonlocal ans
             if r==n:
