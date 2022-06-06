3ae18af07dbd693068b06325c7c918e6b57b288c
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-06-06 22:42:09 +0530

    Time: 2 ms (47.19%), Space: 55.3 MB (43.97%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -13,6 +13,7 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode ptrA = headA;
         ListNode ptrB = headB;
+        // iterating the two linked lists simulaneously, when one pointer reaches the end then make that pointer start iterating through the other linked list; do the same operation for both the pointers until both point at the same node
         while(ptrA!=ptrB){
             if(ptrA!=null){
                 ptrA = ptrA.next;
