8cccf5bcf59da735cf8c18c4f801da64180efdf9
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-06-10 13:08:19 +0530

    Time: 155 ms (72.21%), Space: 14.1 MB (18.82%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,18 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        i=0
+        arr = [0]*26
+        start = 0
         ans = 0
-        n = len(s)
-        dict1 = {} # storing the frequeuncies of all the characters of the window
-        for j in range(n):
-            if s[j] not in dict1:    #checking if the rightmost character is there or not in the window dictonary
-                dict1[s[j]] = 0
-            dict1[s[j]] += 1
-            if j-i+1-max(dict1.values())<=k:  #in each window, the number of times a particular character repeats should be maximum, so that there can be minimum number of replacements of disctinct characters, atmost k times; here, the length of the window is j-i+1 and so, the number of distinct values to be replaced is the expression given in the if condition
-                ans = max(ans,j-i+1)
+        for end in range(len(s)):
+            arr[ord(s[end])-65] += 1
+            len_window = end-start+1
+            distinct_count = len_window - max(arr)
+            if distinct_count<=k:
+                ans = max(ans,len_window)
             else:
-                dict1[s[i]] -= 1      # if the number of replacements exceeds 'k', then remove the leftmost character from the window and decrement the frequency count in the dictionary also
-                if not dict1[s[i]]:  # if the character has zero frequency in the window, then just remove it
-                    del dict1[s[i]]
-                i += 1
+                arr[ord(s[start])-65] -= 1
+                start += 1
         return ans
