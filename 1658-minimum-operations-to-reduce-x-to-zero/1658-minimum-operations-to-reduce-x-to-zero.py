cc95948c2477325e13d964fa402ceed086df12c4
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-06-11 16:53:55 +0530

    Time: 2084 ms (17.23%), Space: 27.7 MB (97.64%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,6 +1,6 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        ans = float("-inf")
+        ans = -1
         start, sum1 = 0,0
         temp = sum(nums)
         for end in range(len(nums)):
@@ -9,8 +9,7 @@ class Solution:
                 sum1 -= nums[start]
                 start += 1
             if sum1==temp-x:
-                #print("here",end)
                 ans = max(ans,end-start+1)
-        if ans==float("-inf"):
+        if ans==-1:
             return -1
         return len(nums)-ans
