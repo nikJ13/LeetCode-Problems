a24d0bc31192ba9e30de2bff5e40cda0a26c1631
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-06-12 22:36:13 +0530

    Time: 1748 ms (54.71%), Space: 27.5 MB (58.21%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,11 +1,11 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         start, ans, sum1 = 0,0,0
-        dict1 = {num:1 for num in nums}
+        dict1 = {num:0 for num in nums}
         for end in range(len(nums)):
-            if dict1[nums[end]]==2:
+            if dict1[nums[end]]==1:
                 ans = max(ans,sum1)
-                while dict1[nums[end]]==2:
+                while dict1[nums[end]]==1:
                     sum1-=nums[start]
                     dict1[nums[start]] -= 1
                     start += 1
