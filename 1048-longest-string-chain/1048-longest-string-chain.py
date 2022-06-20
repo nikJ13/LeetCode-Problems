eb95cda24a5e6c124794eb652c74cbdf61076ce4
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-06-21 00:05:08 +0530

    Time: 219 ms (61.69%), Space: 14.4 MB (65.37%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -5,11 +5,11 @@ class Solution:
         words = sorted(words,key = len)
         for word in words:
             temp[word] = 1
-            for exclude_char in range(len(word)):
-                prev_temp = word[:exclude_char] + word[exclude_char+1:]
-                if prev_temp in temp:
-                    temp[word] = max(temp[word],temp[prev_temp] + 1)
-                    ans = max(ans,temp[word])
+            for exclude_char in range(len(word)): #each character is iterated to consider it to be removed from the current word
+                prev_temp = word[:exclude_char] + word[exclude_char+1:] #after excluding the particular char at the iteration, the rest of the remaining characters of the word form the predecessor
+                if prev_temp in temp: # the predecessor is then searched for in the dictionary
+                    temp[word] = max(temp[word],temp[prev_temp] + 1) # if present, then it equal to max count of chain ending at the current word
+                    ans = max(ans,temp[word]) #updating the max chain ans for the whole array
         #print(temp)
         return ans
                 
