0c8b4b6d9e02cf38705973437da22f1fbd9f8ea8
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2022-07-14 15:13:41 +0530

    Time: 19 ms (57.53%), Space: 41.2 MB (20.48%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,12 +3,12 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left=1;
-        int right=n;
+        int left = 1, right = n;
+        // basic logic is that you first make sure that the right pointer becomes good, then that is where the condition loop breaks, thus, the ans is the left pointer
         while(left<=right){
-            int mid = left+(right-left)/2;
+            int mid = left + (right-left)/2;
             if(isBadVersion(mid)){
-                right = mid-1;
+                right = mid - 1;
             }else{
                 left = mid + 1;
             }
