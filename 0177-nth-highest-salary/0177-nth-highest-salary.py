96c203f43b71fed5d07e70a8c791e867bbfb9714
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-09-24 14:29:59 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    uniques = employee["salary"].drop_duplicates().sort_values(ascending=False)
+    if len(uniques)<N or N<0:
+        return pd.DataFrame({f"getNthHighestSalary({N})": [None]})
+    else:
+        return pd.DataFrame({f"getNthHighestSalary({N})":[uniques.iloc[N-1]]})
