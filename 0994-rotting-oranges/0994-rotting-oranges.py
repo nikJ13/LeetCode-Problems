807920d11d761c094431e5d9a97f398cdc1cd5ea
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-09-25 13:56:30 -0400

    Time: 3 ms (81.34%), Space: 18.1 MB (5.58%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,45 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        n,m = len(grid), len(grid[0])
+        start = 0
+        count_one = 0
+        que = []
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]==1:
+                    count_one = 1
+                if grid[i][j]==2:
+                    que.append((i,j))
+        if count_one==0 and not que:
+            return 0
+        if not que:
+            return -1
+        count = 0
+        while que:
+            k = len(que)
+            for _ in range(k):
+                node = que.pop(0)
+                top = (node[0]-1, node[1])
+                left = (node[0], node[1]-1)
+                bottom = (node[0]+1, node[1])
+                right = (node[0], node[1]+1)
+                if node[0] - 1 >= 0 and grid[node[0]-1][node[1]]!=2 and grid[node[0]-1][node[1]]!=0:
+                    grid[node[0]-1][node[1]] = 2
+                    que.append((node[0]-1,node[1]))
+                if node[1]-1>=0 and grid[node[0]][node[1]-1]!=2 and grid[node[0]][node[1]-1]!=0:
+                    grid[node[0]][node[1]-1] = 2
+                    que.append((node[0],node[1]-1))
+                if node[0]+1<n and grid[node[0]+1][node[1]]!=2 and grid[node[0]+1][node[1]]!=0:
+                    grid[node[0]+1][node[1]] = 2
+                    que.append((node[0]+1,node[1]))
+                if node[1]+1<m and grid[node[0]][node[1]+1]!=2 and grid[node[0]][node[1]+1]!=0:
+                    grid[node[0]][node[1]+1] = 2
+                    que.append((node[0],node[1]+1))
+            if que:
+                count += 1
+        for i in range(n):
+            if 1 in grid[i]:
+                return -1
+        return count
+            
+            
