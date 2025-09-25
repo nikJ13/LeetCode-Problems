249435a89fa3e302aa7e205c8948c2c0b21f7d5f
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-09-24 23:52:09 -0400

    Time: 276 ms (65.1%), Space: 67.1 MB (60.31%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    counts = person.groupby("email").size().reset_index(name="num")
-    duplicated = counts[counts["num"]>1][["email"]]
-    return duplicated
+    counts_df = person.groupby("email").size().reset_index(name="num")
+    res = counts_df[counts_df["num"]>1]["email"]
+    return pd.DataFrame({"Email":res})
