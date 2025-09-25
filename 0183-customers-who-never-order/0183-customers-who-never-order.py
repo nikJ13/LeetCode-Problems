5e322cafa17bfd9b0ea587fb366964447e7a0fca
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-09-25 00:10:16 -0400

    Time: 301 ms (38.07%), Space: 67.6 MB (49.79%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    merged_df = customers.merge(orders, how="left", left_on="id", right_on="customerId")
-    mask = merged_df["customerId"].isna()
-    result_df = merged_df[mask]
-    return result_df[["name"]].rename(columns={"name":"Customers"})
+    merged_df = customers.merge(orders, left_on="id", right_on="customerId", how="left")
+    res = merged_df[merged_df["customerId"].isnull()]["name"].to_frame(name="Customers")
+    return res
+    # print(res)es
