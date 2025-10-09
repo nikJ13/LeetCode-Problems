32d9bceadc9ef4529ff99f756cb8f13b25ba6b26
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-10-09 16:06:20 -0400

    Time: 27 ms (5.19%), Space: 19.1 MB (59.57%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        if not prerequisites:
+            return [_ for _ in range(numCourses)]
+        indegree = [0 for _ in range(numCourses)]
+        adj = {i:[] for i in range(numCourses)}
+        for trans in prerequisites:
+            indegree[trans[0]] += 1
+            if trans[1] not in adj:
+                adj[trans[1]] = []
+            adj[trans[1]].append(trans[0])
+        que = []
+        for i in range(numCourses):
+            if indegree[i]==0:
+                que.append(i)
+        res = []
+        que = deque(que)
+        while que:
+            print(que)
+            node = que.popleft()
+            for neighs in adj[node]:
+                indegree[neighs] -= 1
+                if indegree[neighs]==0:
+                    que.append(neighs)
+            res.append(node)
+        if len(res)!=numCourses:
+            return []
+        return res
+                
+            
+
+            
+
+        
+        
