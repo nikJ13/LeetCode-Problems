cb3082d167c0a869d0f613ff2bfcf7a7fa2ed035
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-10-11 14:13:09 -0400

    Time: 47 ms (75.66%), Space: 26 MB (69.76%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,33 +1,30 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n==1:
+            return [0]
+        
         adj = {i:[] for i in range(n)}
+        degree = [0] * n
         for edge in edges:
             v1, v2 = edge[0], edge[1]
             adj[v1].append(v2)
             adj[v2].append(v1)
+            degree[v1] += 1
+            degree[v2] += 1
         
-        def traverse(node):
-            if node in visited:
-                return 0
-            visited.add(node)
-            if not adj[node]:
-                return 1
-            max_h = 0
-            for neighs in adj[node]:
-                h = traverse(neighs)
-                max_h = max(h, max_h)
-            return max_h + 1
-        ans = []
-        ans_heights = []
-        min_h = float("inf")
-        for roots in adj:
-            visited = set()
-            height = traverse(roots)
-            min_h = min(height, min_h)
-            ans_heights.append((roots, height))
-        
-        for tups in ans_heights:
-            if tups[1]==min_h:
-                ans.append(tups[0])
-        return ans
+        que = deque()
+        for d in range(len(degree)):
+            if degree[d] == 1:
+                que.append(d)
+        remaining = n
+        while remaining>2:
+            k = len(que)
+            remaining -= k
+            for _ in range(k):
+                node = que.popleft()
+                for neighs in adj[node]:
+                    degree[neighs] -= 1
+                    if degree[neighs] == 1:
+                        que.append(neighs)
+        return list(que)
         
