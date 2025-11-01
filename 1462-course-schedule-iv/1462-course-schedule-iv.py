baf7d33d27fafb0422d9ce7cdf72909950e181c4
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-11-01 14:41:51 -0400

    Time: 1302 ms (8.52%), Space: 20.3 MB (82%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,14 +1,11 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         adj = [[] for _ in range(numCourses)]
-        indegrees = [0 for _ in range(numCourses)]
         for prev, next_node in prerequisites:
             adj[prev].append(next_node)
-            indegrees[next_node] += 1
 
 
         def check_pre(next_one, queue, visited):
-            found = 0
             while queue:
                 k = len(queue)
                 for _ in range(k):
@@ -24,10 +21,9 @@ class Solution:
                     
         ans = []
         for prev_q, next_q in queries:
-            temp = indegrees[:] # this makes a new copy of the list because when you slice, python essentially creates new strides for the slice and then makes a new copy of a compact array in the memory.
+            # temp = indegrees[:] # this makes a new copy of the list because when you slice, python essentially creates new strides for the slice and then makes a new copy of a compact array in the memory.
             que = deque([prev_q])
             visited = set()
             visited.add(prev_q)
             ans.append(check_pre(next_q, que, visited))
-        
         return ans
