9e8232247b64136fffa90cb2dc530288ec82b808
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-11-02 11:56:30 -0500

    Time: 18 ms (24.41%), Space: 18.2 MB (48.45%) - LeetHub

---
+++ 3481-apply-substitutions/3481-apply-substitutions.py
@@ -0,0 +1,26 @@
+class Solution:
+    def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
+        adj = {}
+        for key, val in replacements:
+            adj[key] = list(val)
+        ans = ""
+
+        def dfs(ch):
+            temp = ""
+            for neighs in adj[ch]:
+                if neighs=="%":
+                    continue
+                if neighs in adj:
+                    temp += dfs(neighs)
+                else:
+                    temp += neighs
+            return temp
+        
+        for ch in text:
+            if ch=="%":
+                continue
+            if ch in adj:
+                ans += dfs(ch)
+            else:
+                ans += ch
+        return ans
