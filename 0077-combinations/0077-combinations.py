75451c17797f1f980a85036adcb0310015fa1f5d
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-11-20 22:25:49 -0500

    Time: 662 ms (5.04%), Space: 60 MB (10.65%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def helper(idx, arr):
+            print(arr)
+            if len(arr)==k:
+                return arr
+            for p in range(idx+1,n+1):
+                if p not in visited:
+                    visited.add(p)
+                    tmp = helper(p, arr + [p])
+                    if tmp is not None:
+                        ans.append(tmp)
+                    visited.remove(p)
+        visited = set()
+        for i in range(1,n+1):
+            visited.add(i)
+            temp = helper(i ,[i])
+            if temp is not None:
+                ans.append(temp)
+            visited.remove(i)
+        return ans
