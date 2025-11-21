5ab1ab79c4ca8b28b63d438235834c73d7c3979b
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2025-11-20 22:27:43 -0500

    Time: 284 ms (6.49%), Space: 59.8 MB (27.13%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,7 +2,6 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans = []
         def helper(idx, arr):
-            print(arr)
             if len(arr)==k:
                 return arr
             for p in range(idx+1,n+1):
