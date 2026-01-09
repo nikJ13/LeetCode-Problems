e1219fcd947846e3b39c57882ef34a87c728c80d
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2026-01-09 16:52:24 -0500

    Time: 3 ms (99.17%), Space: 23.3 MB (5.01%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,23 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        pq = []
-        counter = 0
-        for node in lists:
-            if node:
-                counter += 1
-                pq.append((node.val, counter, node))
-        heapq.heapify(pq)
-        head_before = ListNode()
-        prev = head_before
-        while pq:
-            # print(pq)
-            value, c, node_ = heapq.heappop(pq)
-            curr = ListNode(value)
-            prev.next = curr
-            prev = prev.next
-            if node_.next != None:
-                heapq.heappush(pq, (node_.next.val, c, node_.next))
-        return head_before.next
+        # make a dict with keys being the head of each linked list and value as the index in the bigger list
+        # maintain a heapq and pop the smallest elements
+        # add the head of that same linked list in the heapq
+
+        # [[1,2,2],[1,1,2]]
+        #     .         .
+        if len(lists)==0:
+            return None
+        queue = []
+        for i in range(len(lists)):
+            linked_head = lists[i]
+            if linked_head:
+                queue.append((linked_head.val,i))
+        heapq.heapify(queue)
+        temp = ListNode(-1)
+        curr = temp
+        while len(queue)!=0:
+            #print(queue)
+            smallest_head, idx = heapq.heappop(queue)
+            smallest_head_lc = lists[idx]
+            if smallest_head_lc.next is not None:
+                heapq.heappush(queue,(smallest_head_lc.next.val,idx))
+            lists[idx] = smallest_head_lc.next
+            smallest_head_lc.next = None
+            curr.next = smallest_head_lc
+            curr = curr.next
+        return temp.next
+        
