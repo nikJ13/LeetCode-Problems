1832414e9624a36dddf41d07133a222b74150475
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2026-02-08 11:14:32 -0500

    Time: 3 ms (75.54%), Space: 20.6 MB (34.23%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,37 @@
+from collections import deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        if not prerequisites:
-            return [_ for _ in range(numCourses)]
-        indegree = [0 for _ in range(numCourses)]
         adj = {i:[] for i in range(numCourses)}
-        for trans in prerequisites:
-            indegree[trans[0]] += 1
-            if trans[1] not in adj:
-                adj[trans[1]] = []
-            adj[trans[1]].append(trans[0])
-        que = deque()
-        for i in range(numCourses):
-            if indegree[i]==0:
-                que.append(i)
-        res = []
-        while que:
-            node = que.popleft()
-            for neighs in adj[node]:
-                indegree[neighs] -= 1
-                if indegree[neighs]==0:
-                    que.append(neighs)
-            res.append(node)
-        if len(res)!=numCourses:
+        indegree = [0 for _ in range(numCourses)]
+        for edges in prerequisites:
+            parent, child = edges[1],edges[0]
+            adj[parent].append(child)
+            indegree[child] += 1
+        """
+        [[1,0],[1,2],[0,1]]
+        0->1
+        2->1
+        1->0
+        [1,1,0]
+        [2]
+        """
+        # we need to find which courses are parents of all courses; indegree of 0
+        queue = deque([i for i in range(numCourses) if indegree[i]==0])
+        print(queue)
+        visited = set()
+        ans = []
+        while queue:
+            course = queue.popleft()
+            if course in visited:
+                return []
+            for child in adj[course]:
+                indegree[child] -= 1
+                if indegree[child]==0:
+                    queue.append(child)
+            visited.add(course)
+            ans.append(course)
+        if len(ans)!=numCourses:
             return []
-        return res
-                
+        return ans
             
 
-            
-
-        
-        
