b7d155c4d9d1a434a48b089ff4ce3258d9227fd6
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2026-02-08 13:16:35 -0500

    Time: 12 ms (15.89%), Space: 23.1 MB (23.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,31 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        for n in nums:
-            if n not in freq:
-                freq[n] = 0
-            freq[n] += 1
-        max_f = float("-inf")
-        for f in freq:
-            if freq[f]>max_f:
-                max_f = freq[f]
+        """
+        Problem: return the k most frequent elements
+        Solution:
+        Naive => keep track of the frequencies of the numbers and then use a heapq to store the top k most frequent numbers
+        finally get all the numbers in the heapq
+        But we want something better than O(nlogn)
+        """
+        dict_freq = {}
+        for i in range(len(nums)):
+            if nums[i] not in dict_freq:
+                dict_freq[nums[i]] = 0
+            dict_freq[nums[i]] += 1
+        queue = []
+        heapq.heapify(queue)
+        # [-1,4]
+        for key in dict_freq:
+            if len(queue)<k:
+                heapq.heappush(queue,(dict_freq[key],key))
+            else:
+                freq, head = heapq.heappop(queue)
+                if freq < dict_freq[key]:
+                    heapq.heappush(queue,(dict_freq[key],key))
+                else:
+                    heapq.heappush(queue,(freq,head))
         ans = []
-        curr_f = max_f
-        while k>0:
-            for f in freq:
-                if freq[f]==curr_f and k>0:
-                    k -= 1
-                    ans.append(f)
-            curr_f -= 1
+        for f,a in queue:
+            ans.append(a)
         return ans
