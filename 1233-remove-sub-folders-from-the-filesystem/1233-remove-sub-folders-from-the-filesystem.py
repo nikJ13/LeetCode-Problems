867fa83f2028e0ee6b887529ba0fc5ebef7d104a
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2026-02-12 17:08:54 -0500

    Time: 19 ms (92.65%), Space: 33.1 MB (37.21%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -23,41 +23,12 @@ class Solution:
         we might need some sort of a tree traversal
         trie would work here
         """
-        dict_folders = {}
+        folder.sort()
 
-        def build(dict_f, arr):
-            if len(arr)==0:
-                return
-            if arr[0] not in dict_f:
-                dict_f[arr[0]] = {}
-            build(dict_f[arr[0]],arr[1:])
-        
-        def check(dict_f, arr):
-            if len(arr)==0: # if the string ends, that is the parent then, return empty dict
-                return {}
-            if arr[0] not in dict_f: # if it has wiped clean then return empty dict
-                return {}
-            dict_f[arr[0]] = check(dict_f[arr[0]],arr[1:])
-            return dict_f
-
-        for f in folder:
-            f_arr = f.split("/")[1:]
-            build(dict_folders, f_arr)
-        print(dict_folders)
-        for f in folder:
-            f_arr = f.split("/")[1:]
-            dict_folders = check(dict_folders, f_arr)
-        
-        ans = []
-
-        def traverse(dict_f,path):
-            if len(dict_f)==0:
-                ans.append(path[:-1])
-                return
-            for keys in dict_f:
-                traverse(dict_f[keys],path+keys+"/")
-
-        traverse(dict_folders,"/")
+        ans = [folder[0]]
+        for f in folder[1:]:
+            last = ans[-1]
+            if not f.startswith(last) or f[len(last)]!="/":
+                ans.append(f)
         return ans
-
 
