492f13a9208b4a6417ce3b05318d97a484c39f80
Niket Jain <51831161+nikJ13@users.noreply.github.com>
2026-03-09 16:20:22 -0400

    Time: 3 ms (11.21%), Space: 19.4 MB (58.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,31 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def quick_sort(arr,left,right):
-            if right<=left:
+        """
+        quick sort is the one algo that can do this sorting in place
+
+        2 3 8 4 6
+          l     r
+        2 6 8 4 3  
+        """
+        def calculate_pivots(start, end):
+            if start >= end:
                 return
-
-            pivot_idx = calculate_partition(arr,left,right)
-
-            quick_sort(arr,left,pivot_idx-1)
-
-            quick_sort(arr,pivot_idx+1,right)
-
-            return arr
-
-        def calculate_partition(arr,left,right):
-            pivot = arr[right]
-            i = left
-
-            for j in range(left,right):
-                if arr[j]<=pivot:
-                    arr[i], arr[j] = arr[j], arr[i]
-                    i += 1
-            
-            # swap the pivot with the element at the ith idx
-            arr[i], arr[right] = arr[right], arr[i]
-            return i
-
-        return quick_sort(nums,0,len(nums)-1)
-
-
+            # we need to decide the pivots
+            new_pivot_idx = iterations(start, end)
+            calculate_pivots(start, new_pivot_idx-1)
+            calculate_pivots(new_pivot_idx+1, end)
+            return
+        
+        def iterations(start, end):
+            left = start
+            curr_pivot = nums[end]
+            for right in range(start, end):
+                if nums[right] <= curr_pivot:
+                    nums[left], nums[right] = nums[right], nums[left]
+                    left += 1
+            nums[left], nums[end] = nums[end], nums[left]
+            return left
+        
+        calculate_pivots(0,len(nums)-1)
+        return nums
